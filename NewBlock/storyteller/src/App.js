c81b15b92d87aa1ae9b7afac4ab9cb21724bf4e0
Subhratanu <subhratanusaha100@gmail.com>
2025-04-13 17:14:45 +0530

    state variable introduced

--- NewBlock/storyteller/src/App.js
+++ NewBlock/storyteller/src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 import Cards from './Components/Cards';
 import mockSampleData from "./Mockdata/mockSampleData.json"
 function App() {
-  console.log(mockSampleData);
-  console.log(mockSampleData.users);
+  //using state variable -> Re Render our Applications
+  // useState() is a function/ React Hook
+  // useState() returns an Array consist of primary value & function
+  const [value,setValue] = useState(10);
+  const incrementLogic=()=>{
+    setValue(value+1);
+    console.log("Increment Logic called: ",value);
+  }
+
+  const decrementLogic=()=>{
+    setValue(value-1);
+    console.log("Decrement Logic called: ",value);
+  }
   return (
     <div>
       <h1> This is my First ever React Application</h1>
+      <div className="cart-func">
+      <button className="btn-primary" onClick={decrementLogic}>Decrement(-)</button>
+      <h3>{value}</h3>
+      <button className="btn-primary" onClick={incrementLogic}>Increment(+)</button>
+      </div>
       {
         // Calling the Cards component
       }
